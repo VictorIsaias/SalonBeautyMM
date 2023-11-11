821f24648c1075952b1f9fd937f5ef967746ffce
VictorIsaias <isaias.castrejonrdz@gmail.com>
2023-11-10 20:47:15 -0600

    versión 0.1

--- SalonBeautyMM/src/router/index.js
+++ SalonBeautyMM/src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import iniciarview from '../views/IniciarView.vue'
+import registrarview from '../views/Registrarview.vue'
+import administrarview from '../views/Administrarview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Principal',
+      component: iniciarview  // Cambiar a componente de pagina principal
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/iniciar',
+      name: 'iniciar',
+      component: iniciarview
+    },
+    {
+      path: '/registrar',
+      name: 'registrar',
+      component: registrarview
+    },
+    {
+      path: '/Administrar',
+      name: 'Administrar',
+      component: administrarview
     }
   ]
 })
