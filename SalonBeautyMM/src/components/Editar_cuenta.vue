cbff4cca2443eae6ac20b5e25b5f9c6e66eb91fc
denixxe <denissegarciaramirez@gmail.com>
2023-12-10 17:01:41 -0600

    Contacto actualizado y responsivo, se resolvieron detallitos

--- SalonBeautyMM/src/components/Editar_cuenta.vue
+++ SalonBeautyMM/src/components/Editar_cuenta.vue
@@ -1,16 +1,17 @@
 <template>
-  <v-container class="pa-9" align="center" justify="center">
-              <v-card align="start" justify="center" width="650" height="550" style="font-family:'Britannic'" color="#FFE5FD">            
+  <v-container class="pa-9 " align="center" justify="center">
+              <v-card class="contenedor" align="start" justify="center"  style="font-family:'Britannic'" color="#FFE5FD">            
         <v-form  @submit.prevent="submitForm">   
           <v-row justify="center">  
             <v-col style="font-family:'Oswald'" cols="11">
               <v-card-title class="titulo" style="font-family:'Britannic'">Mi cuenta</v-card-title>
-              <v-divider class="mx-4 mb-6 border-opacity-25" ></v-divider> 
-              <v-text-field variant="outlined" v-model="datos.nombre" label="Nombre/s" ></v-text-field>
-              <v-text-field variant="outlined" v-model="datos.apellido" label="Apellido/s"></v-text-field>
-              <v-text-field variant="outlined" v-model="datos.telefono" label="Numero de teléfono"></v-text-field>
-              <v-text-field variant="outlined" v-model="datos.email" label="Correo Electrónico *" type="email"></v-text-field>
-              <v-text-field variant="outlined" v-model="datos.contraseña" label="Contraseña *" type="password"></v-text-field>   
+              <v-divider class="mx-4 mb-5 border-opacity-25" ></v-divider> 
+              <v-text-field class="texto" variant="outlined" v-model="datos.nombre" label="Nombre/s" ></v-text-field>
+              <v-text-field class="texto" variant="outlined" v-model="datos.apellido_p" label="Apellido paterno"></v-text-field>
+              <v-text-field class="texto" variant="outlined" v-model="datos.apellido_m" label="Apellido materno"></v-text-field>
+              <v-text-field class="texto" variant="outlined" v-model="datos.telefono" label="Numero de teléfono"></v-text-field>
+              <v-text-field class="texto" variant="outlined" v-model="datos.email" label="Correo Electrónico *" type="email"></v-text-field>
+              <v-text-field class="texto" variant="outlined" v-model="datos.contraseña" label="Contraseña *" type="password"></v-text-field>   
               <v-btn class="boton" align="center" justify="center" type="submit" elevation="6">Actualizar</v-btn>
             </v-col>     
           </v-row>     
@@ -24,7 +25,10 @@
       background: linear-gradient(to top, #ff9a9e 0%, #fecfef 99%, #fecfef 100%);
       color: purple;
       font-family: "Britannic";
+      margin-bottom: 20px;
     }
+   
+    
     </style>
     <script>
   
@@ -34,7 +38,8 @@
         return {
           datos: {
             nombre: '',
-            apellido: '',
+            apellido_p: '',
+            apellido_m:'',
             email: '',
             telefono: '',
             contraseña: ''
@@ -46,10 +51,12 @@
           // Aquí puedes manejar la lógica de envío del formulario
           console.log('Cambios guardados');
           console.log('Nuevo Nombre:', this.datos.nombre);
-          console.log('Nuevo Apellido:', this.datos.apellido);
+          console.log('Nuevo Apellido Paterno:', this.datos.apellido_p);
+          console.log('Nuevo Apellido Materno:', this.data.apellido_m);
           console.log('Nuevo telefono:', this.datos.telefono);
           console.log('Nuevo Correo Electrónico:', this.datos.email);
         }
       }
     };
     </script>
+  
