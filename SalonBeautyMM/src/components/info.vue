2219897c810c7b476ed13bcd7e93b8f99a15d858
denixxe <denissegarciaramirez@gmail.com>
2023-12-10 11:21:55 -0600

    Contacto actualizado y responsivo

--- SalonBeautyMM/src/components/info.vue
+++ SalonBeautyMM/src/components/info.vue
@@ -1,23 +1,24 @@
 <template>
-    <v-container class="d-flex" aling="start" justify="start">
+    <v-container class="d-flex" align="start" justify="start">
         
-  <v-card class="titulo" title="Contacto" width="400" height="450" color="#FDE6DB" style="font-family:'Britannic'">
-    <v-divider class="mx-4 mb-1"></v-divider>
+  <v-card class="titulo" title="Contacto" width="400" height="450" color=#FFE5FD style="font-family:'Britannic'">
+    <v-divider class="mx-4 mb-6 border-opacity-25"></v-divider>
     <v-card-item style="font-family:'Oswald'">
         
-        <v-card-subtitle>Numero de telefono</v-card-subtitle>
+        <v-card-subtitle class="font-weight-bold">Numero de telefono</v-card-subtitle>
        <v-list-item  density="compact">
-         
         <v-list-item-subtitle v-for="telefono in telefonos" :key="telefono">{{ telefono.numero }}</v-list-item-subtitle></v-list-item>
-       
+          
         <v-card-subtitle class="font-weight-bold">Correo electronico</v-card-subtitle>
         <v-list-item  density="compact">
         <v-list-item-subtitle v-for="correo in correos" :key="correo">{{ correo.texto }}</v-list-item-subtitle></v-list-item>
-
+        
         <v-card-subtitle class="font-weight-bold">Pagina de Facebook</v-card-subtitle>
         <v-list-item  density="compact">
         <v-list-item-subtitle v-for="pagina in paginas" :key="pagina">{{ pagina.texto }}</v-list-item-subtitle>
+
         </v-list-item>
+    
         </v-card-item>
 
   </v-card>
